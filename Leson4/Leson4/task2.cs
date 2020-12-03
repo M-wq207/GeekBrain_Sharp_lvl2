30a212174bb12fb2f690200e07f5ef18b06ea4ce
M-wq207 <grekus777@mail.ru>
2020-12-03 14:10:23 +0300

    2. Дана коллекция List<T>, требуется подсчитать, сколько раз каждый элемент встречается в данной коллекции: а) для целых чисел; б) *для обобщенной коллекции; в) *используя Linq.
    3. *Дан фрагмент программы:
    Dictionary<string, int> dict = new Dictionary<string, int>()
      {
        {"four",4 },
        {"two",2 },
        { "one",1 },
        {"three",3 },
      };
         var d = dict.OrderBy(delegate(KeyValuePair<string,int> pair) { return pair.Value; });
         foreach (var pair in d)
        {
          Console.WriteLine("{0} - {1}", pair.Key, pair.Value);
        }
            а) Свернуть обращение к OrderBy с использованием лямбда-выражения $.
            не смог сделать (б) *Развернуть обращение к OrderBy с использованием делегата Predicate<T>.), не понял куда именно прикрутить метод подходящий делегату Predicate

---
+++ Leson4/Leson4/task2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Leson4
+{
+    //2. Дана коллекция List<T>, требуется подсчитать, сколько раз каждый элемент встречается в данной коллекции:
+    //а) для целых чисел;
+    //б) * для обобщенной коллекции;
+    //в) * используя Linq.
+    class task2
+    {   
+        //a)коллекция целых чисел
+        public static List<int> myList0 = new List<int>() { 1, 2, 3, 4, 5, 234, 43, 2, 1, 3, 2, 4, 6, 23 };
+        public static List<string> getElementCountInList<T>(List<T> myList0)
+        {
+            List<string> resultList = new List<string>();
+            foreach (var a in myList0)
+            {
+                int counter = 0;
+
+                foreach (var b in myList0)
+                {
+                    try
+                    {
+                        if (a.Equals(b)) counter++;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("несравнимые элементы");
+                    }
+                }
+                string c = $"Элемент {a} встречается в листе {counter} раз(а)";
+                if (!resultList.Contains(c)) resultList.Add(c);
+            }
+            return resultList;
+        }
+        //б)обобщенная коллекция? (List<T> и есть обобщенная коллекнция, не до конца понял задание)
+        public static List<object> myList1 = new List<object>() { "text", 1, 23.1, myList0, 3, "text" };
+    }
+}
